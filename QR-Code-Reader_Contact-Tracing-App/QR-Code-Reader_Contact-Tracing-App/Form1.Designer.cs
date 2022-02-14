2d5a0a2161f0ea31989ab058cfd4c4cad1a620e9
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-02-14 13:15:12 +0800

    Initial commit. WIP, but can read text from QR code.

---
+++ QR-Code-Reader_Contact-Tracing-App/QR-Code-Reader_Contact-Tracing-App/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace QR_Code_Reader_Contact_Tracing_App
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboBxDevices = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.displayTxtBx = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Camera:";
+            // 
+            // cboBxDevices
+            // 
+            this.cboBxDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBxDevices.FormattingEnabled = true;
+            this.cboBxDevices.Location = new System.Drawing.Point(33, 55);
+            this.cboBxDevices.Name = "cboBxDevices";
+            this.cboBxDevices.Size = new System.Drawing.Size(323, 21);
+            this.cboBxDevices.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(33, 97);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(445, 407);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(33, 526);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(213, 44);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // displayTxtBx
+            // 
+            this.displayTxtBx.Location = new System.Drawing.Point(519, 232);
+            this.displayTxtBx.Multiline = true;
+            this.displayTxtBx.Name = "displayTxtBx";
+            this.displayTxtBx.Size = new System.Drawing.Size(428, 80);
+            this.displayTxtBx.TabIndex = 4;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(270, 526);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 44);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "S&top";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 594);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.displayTxtBx);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.cboBxDevices);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contact Tracing App Using QR Code";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboBxDevices;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox displayTxtBx;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ QR-Code-Reader_Contact-Tracing-App/QR-Code-Reader_Contact-Tracing-App/Form1.cs
@@ -0,0 +1,93 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QR_Code_Reader_Contact_Tracing_App
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+
+        bool isCameraRunning = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo x in filterInfoCollection)
+            {
+                cboBxDevices.Items.Add(x.Name);
+                cboBxDevices.SelectedIndex = 0;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[cboBxDevices.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+
+            isCameraRunning = true;
+
+            timer.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            pictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isCameraRunning)
+            {
+                captureDevice.Stop();
+            } 
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)pictureBox.Image);
+                if (result != null)
+                {
+                    displayTxtBx.Text = result.ToString();
+                    timer.Stop();
+                    if (isCameraRunning)
+                    {
+                        captureDevice.Stop();
+                    }
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (captureDevice.IsRunning)
+                captureDevice.Stop();
+
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image = null;
+            }
+
+            isCameraRunning = false;
+        }
+    }
+}
